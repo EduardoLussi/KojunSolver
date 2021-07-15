fa9c73ecb3a8519dd149445a0ab7fd0622110220
Anthony Kamers <anthonybernardo10@gmail.com>
2021-07-15 14:30:51 -0300

    first commit -> grade de jogo formada pelo input

---
+++ main.hs
@@ -0,0 +1,22 @@
+module Main (main) where
+
+-- import Parse -- parse do arquivo de input
+import System.IO -- args do usuário (caso precisar)
+import Data.List.Split -- split de strings
+import Parse -- fazer parse do arquivo de input e montar Kojun
+
+main = do
+    let file = "input.txt"
+    str <- readFile file
+    let stringList = splitOn "\n" str
+
+    let size = stringList !! 0
+    let sizeNumber = read size :: Int
+
+    let listNumber = parseFile (tail stringList) sizeNumber
+    
+    let kojum = firstNelements listNumber sizeNumber
+    let grupos = lastNelements listNumber sizeNumber
+
+    print(kojum)
+    print(grupos)
